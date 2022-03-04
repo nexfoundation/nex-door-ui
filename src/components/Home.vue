f1053f3797bd0cc8ff9a22c15dc3f853a8a6a83a
Eason Cao <eason@easoncao.com>
2022-03-04 19:09:44 +0000

    Allow update personal profile

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,15 +4,21 @@
         <p>Amplify your apps. Build on a flexible, scalable, and reliable serverless backend.</p>
         <div class="card-columns">
             <div class="card" v-for="user in users">
-                <div class="card-body">
+                <div class="card-body" :set="user_attributes = getAttribute(user['Attributes'])">
                     <div class="media">
-                        <img src="http://via.placeholder.com/64x64" class="mr-3">
+                        <img :src="'https://www.gravatar.com/avatar/' + user_attributes['picture'] + '?s=80'" class="mr-3">
                         <div class="media-body">
-                            <h5 class="mt-0">{{ user['Username'] }}</h5>
-                            Profile-1, Profile-2
+                            <h5 class="mt-0">{{ user_attributes['name'] }} </h5>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit
                         </div>
                     </div>
-                    <p class="card-text"><code>{{ JSON.stringify(user['Attributes']) }}</code></p>
+                    <hr>
+                    <p><a :href="user_attributes['custom:calendy_url']" :disabled="! user_attributes['custom:calendy_url']" class="btn btn-success">Make an appointment</a></p>
+                    <p class="card-text">
+                        {{ user['Username'] }}
+                        {{ user_attributes['profile'] }}
+                    </p>
+                    <code>{{ user['Attributes'] }}</code>
                 </div>
             </div>
         </div>
@@ -30,6 +36,14 @@ export default {
         return { users: undefined }
     },
     methods: {
+        getAttribute(user_attributes) {
+            let attributes = {};
+            for(let i of user_attributes){
+                attributes[i["Name"]] = i["Value"];
+            }
+
+            return attributes;
+        },
         getData() {
             const apiName = 'ServiceEndpoint'
             const path = '/query'
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,36 +1,123 @@
 <template>
-  <div class='container'>
-    <h1>Yo, {{ username }}</h1>
-    <p>Amplify your apps. Build on a flexible, scalable, and reliable serverless backend.</p>
-  </div>
+    <div class='container'>
+        <h1>Yo, {{ username }}</h1>
+        <p>Amplify your apps. Build on a flexible, scalable, and reliable serverless backend.</p>
+        <code>{{ user_attributes }}</code>
+        <div id='profile-form'>
+            <form>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="text" readonly class="form-control-plaintext" :value="user_attributes['email']">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputName" placeholder="Name" :value="user_attributes['name']">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputProfilePhoto" class="col-sm-2 col-form-label">Profile photo (Gravatar)</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputProfilePhoto" placeholder="205e460b479e2e5b48aec07710c08d50" :value="user_attributes['picture']">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputWebsite" class="col-sm-2 col-form-label">Website</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputWebsite" placeholder="https://example.com" :value="user_attributes['website']">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputCalendy" class="col-sm-2 col-form-label">Calendy</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputCalendy" placeholder="https://calendly.com/<username>" :value="user_attributes['custom:calendy_url']">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputProfileBio">Bio</label>
+                    <textarea class="form-control" id="inputProfileBio" rows="3" v-model="user_attributes['profile']"></textarea>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button class="btn btn-primary" v-on:click="updateAttribute">Update</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
+    export default {
+        name: 'home',
+        async beforeCreate() {
+            try {
+                let user = await this.$Amplify.Auth.currentAuthenticatedUser()
+                let { attributes } = user
 
-export default {
-  name: 'home',
-  computed: {
-    username() {
-      return this.$store.state.user.username
+                console.log(attributes)
+                this.user_attributes = attributes
+            } catch (err) {
+                console.log('error: ', err)
+            }
+        },
+        data() {
+            return {
+                user_attributes: undefined
+            }
+        },
+        computed: {
+            username() {
+                return this.$store.state.user.username
+            }
+        },
+        methods: {
+            async updateAttribute() {
+                try {
+                    let user = await this.$Amplify.Auth.currentAuthenticatedUser()
+                    let result = await this.$Amplify.Auth.updateUserAttributes(user, {
+                        'name': document.getElementById("inputName").value,
+                        'picture': document.getElementById("inputProfilePhoto").value,
+                        'profile': document.getElementById("inputProfileBio").value,
+                        'website': document.getElementById("inputWebsite").value,
+                        'custom:calendy_url': document.getElementById("inputCalendy").value
+                    })
+
+                    console.log(result)
+
+                    // Refresh local current user session and state
+                    user = await this.$Amplify.Auth.currentAuthenticatedUser({ bypassCache: true })
+                    this.$store.dispatch('setUser', user)
+                    this.$router.push('home')
+                } catch (err) {
+                    console.log('error: ', err)
+                }
+            }
+        }
     }
-  }
-}
 </script>
 
 <style scoped>
-.container {
-  padding-top: 80px;
-  width: 800px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-}
-.container h1 {
-  margin-bottom: 0px;
-}
-.container p {
-  font-size: 18px;
-  text-align: left;
-}
+    #profile-form {
+        width: 100%;
+    }
+    .container {
+        padding-top: 80px;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+    .container h1 {
+        margin-bottom: 0px;
+    }
+    .container p {
+        font-size: 18px;
+        text-align: left;
+    }
+    .container textarea {
+        text-align: left;
+    }
 </style>
