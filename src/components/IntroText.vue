688a2baddf043c3ca093a07ed2e0f73caaf449e1
ytkuo <hahaeating1@gmail.com>
2023-01-04 19:36:12 -0500

    make http link clickable in introduction section

---
+++ src/components/IntroText.vue
@@ -0,0 +1,19 @@
+<template>
+	<a v-if="str.includes('http')" :href="str">{{str}}</a>
+	<p v-else-if="str">{{str}}</p>
+	<br v-else />
+</template>
+
+<script>
+export default {
+	name: 'intro-text',
+	props: ['str']
+}
+</script>
+
+<style>
+	pre.intro p {
+		text-align: left;
+		margin: 0;
+	}
+</style>
