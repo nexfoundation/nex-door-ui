f5c10c80df2fa9efec1d3d3deb94e76d9ebd3125
Jack Tsai <jtsai0606@gmail.com>
2023-03-11 20:54:08 +0000

    no need for separate css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{vue,js,ts}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
     container: {
