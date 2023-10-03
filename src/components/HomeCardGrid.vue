c3e22df1a4f291e4b803a4c47880b6dc71682158
Chunyao Chan <24861096+Civon@users.noreply.github.com>
2023-10-03 23:17:33 +0800

    NEX-190/tag-filter (#43)
    * tags filter with single tag
    
    country filter TBD, toggle on/off to filter TBD
    
    * wip: country select in profile (untest commit)
    
    * tags filter with single tag
    
    country filter TBD, toggle on/off to filter TBD
    
    * wip: country select in profile (untest commit)
    
    * fix: typo
    
    * hotfix: eslint
    
    * country filter works, able to re-toggle
    
    * chore: refactor name of filter component
    
    * style: filter placeholder
    
    * fix: cate. button can be toggled
    
    * feat: no result hints
    
    * fix: filter logic, tolerant null country code
    
    * feat: avoid layout shift (visual change) when filter
    
    card grid component set to min height 80vh
    
    * style: clean up
    
    * chore: npm lock

--- src/components/HomeCardGrid.vue
+++ src/components/HomeCardGrid.vue
@@ -1,14 +1,19 @@
 <template>
-  <div
-    v-if="state.users"
-    class="grid grid-cols-1 lg:grid-cols-3 gap-4 my-12"
-  >
-    <HomeCard
-      v-for="user in usersAcceptMentoring"
-      :key="user.sub"
-      :user="user"
-      @show-modal="$emit('showModal', user)"
-    />
+  <div>
+    <div class="min-h-[80vh]">
+      <HomeCardGridFilterCountry @selectedTags-updated="handleSelectedCountryTagsUpdate"></HomeCardGridFilterCountry>
+      <HomeCardGridFilterTags @selectedTags-updated="handleSelectedTagsUpdate"></HomeCardGridFilterTags>
+
+      <div v-if="state.users" class="grid grid-cols-1 lg:grid-cols-3 gap-4 my-12">
+        <HomeCard v-for="user in usersFiltered" :key="user.sub" :user="user" @show-modal="$emit('showModal', user)" />
+        <div v-if="usersFiltered.length == 0">
+          <p>找不到符合條件的導師呦🫠</p>
+        </div>
+      </div>
+      <div v-else>
+        <p>資料庫壞了😥</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,13 +22,18 @@ import { reactive, computed } from 'vue';
 import { API } from 'aws-amplify';
 import { UserAttributes } from '../constants';
 import HomeCard from './HomeCard';
+import HomeCardGridFilterTags from "./HomeCardGridFilterTags.vue";
+import HomeCardGridFilterCountry from './HomeCardGridFilterCountry.vue';
 
 const apiName = 'ServiceEndpoint';
 const path = '/query';
-
 const state = reactive({
   users: [],
   modalCurrentUser: undefined,
+  filters: {
+    country: '',
+    tags: ''
+  }
 });
 
 try {
@@ -38,10 +48,27 @@ try {
 } catch (error) {
   console.error(error);
 }
+const handleSelectedTagsUpdate = (selectedTags) => {
+  state.filters.tags = selectedTags
+}
+const handleSelectedCountryTagsUpdate = (selectedCountryObj) => {
+  state.filters.country = selectedCountryObj
+}
 
-const usersAcceptMentoring = computed(() =>
-  state.users.filter((u) => u[UserAttributes.ACCEPT_MENTORING] === '1')
-);
+const usersFiltered = computed(() => {
+  return state.users.filter((u) => {
+    // Check if the user accepts mentoring and includes the necessary tags
+    const baseCheck = u[UserAttributes.ACCEPT_MENTORING] === '1' && u[UserAttributes.TAGS]?.includes(state.filters.tags);
+
+    // If country filter is null, ignore the country code check
+    if (!state.filters.country) {
+      return baseCheck;
+    }
+
+    // If country filter is not null, include the country code check
+    return baseCheck && u[UserAttributes.COUNTRY_CODE] == state.filters.country.value;
+  });
+});
 
 defineEmits(['showModal']);
 </script>
---
+++ src/components/HomeCardGridFilterTags.vue
@@ -0,0 +1,44 @@
+<template lang="">
+  <div>
+    <div class="">
+      <button class="btn btn-outline mr-2 hover:bg-blue-900 focus:shadow-outline"
+      v-for="option in availableTags"  :key="option" @click="selectOption(option)"
+      :class="{ 'bg-blue-500': selectedTag === option }"
+      >{{ option }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTag: '',
+      availableTags: [
+        '稅務簽證',
+        '職涯發展',
+        '商業創業',
+        '租屋買房',
+        '旅行生活',
+        '人生相談',
+        '興趣分享',
+        '設計美學',
+        '海外婚姻',
+      ],
+    }
+  },
+  methods: {
+    selectOption(option) {
+      // ?adhoc: fix empty data from cognito 
+      if (this.selectedTag == option) {
+        this.selectedTag = ''
+        this.$emit('selectedTags-updated', []);
+      }
+      else {
+        this.selectedTag = option;
+        this.$emit('selectedTags-updated', this.selectedTag);
+      }
+    },
+  },
+}
+</script>
