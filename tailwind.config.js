c198a251b8c2c0e2b4cbca5eebb778142a836ad6
Allen Chan <24861096+Civon@users.noreply.github.com>
2024-03-19 13:29:19 +0000

    style: define font prototype

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      sm: ['14px', '140%'],
+      base: ['16px', '140%'],
+      lg: ['18px', '140%'],
+      xl: ['22px', '125%'],
+      '2xl': ['24px', '125%'],
+      '3xl': ['32px', '125%'],
+      'caption': ['12px', '140%']
+    },
     extend: {
       fontFamily: {
-        body: ["Montserrat", "Noto Sans TC", "sans-serif"],
+        body: ["Montserrat", "Noto Sans TC"],
       },
     },
     container: {
