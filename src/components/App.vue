e4019a524adf3a5355ccf3c92c86166e64680372
Chunyao Chan <24861096+Civon@users.noreply.github.com>
2024-03-25 04:36:56 +0800

    style: add font family/size, favicon (#62)
    * style: add font family
    
    * style: define font prototype
    
    * style: homepage's line height and font weight  except search component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col min-h-screen font-body">
     <div class="flex-grow">
       <AppNavbar />
       <router-view />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ['14px', '140%'],
+      base: ['16px', '140%'],
+      lg: ['18px', '140%'],
+      xl: ['22px', '125%'],
+      '2xl': ['24px', '125%'],
+      '3xl': ['32px', '125%'],
+      'display': ['48px', '125%'],
+      'caption': ['12px', '140%']
+    },
+    extend: {
+      fontFamily: {
+        body: ["Montserrat", "Noto Sans TC"],
+      },
+    },
     container: {
       center: true,
     },
