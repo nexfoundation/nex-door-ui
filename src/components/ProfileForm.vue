449ba8d05e44338be5af85bf5d5bc47b91336ea1
Allen Chan <24861096+Civon@users.noreply.github.com>
2024-04-02 16:17:29 +0000

    lint: format using vue extention, and clear comments

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -1,173 +1,58 @@
 <template>
   <div>
-    <div
-      v-if="state.errorMessage"
-      class="alert alert-error"
-      role="alert"
-    >
+    <div v-if="state.errorMessage" class="alert alert-error" role="alert">
       {{ state.errorMessage }}
     </div>
-    <Form
-      v-slot="{ meta, isSubmitting }"
-      :initial-values="formValues"
-      @submit="onSubmit"
-    >
-      <BaseInput
-        name="username"
-        label="用戶名稱"
-        disabled
-      />
-
-      <BaseInput
-        name="email"
-        label="Email"
-        disabled
-      />
-
-      <BaseInput
-        id="name"
-        label="名稱 (必填)"
-        name="name"
-        placeholder="Name"
-        required
-        rules="required"
-      />
-
+    <Form v-slot="{ meta, isSubmitting }" :initial-values="formValues" @submit="onSubmit">
+      <BaseInput name="username" label="用戶名稱" disabled />
+      <BaseInput name="email" label="Email" disabled />
+      <BaseInput id="name" label="名稱 (必填)" name="name" placeholder="Name" required rules="required" />
 
       <div class="form-control w-full max-w-xs">
-        <label
-          for="picture"
-          class="label cursor-pointer"
-        >
+        <label for="picture" class="label cursor-pointer">
           <span class="label-text">
             個人頭像
           </span>
         </label>
-        <Field
-          v-slot="{ handleChange, handleBlur }"
-          name="pictureFile"
-          rules="maxFileSize"
-        >
-          <input
-            id="picture"
-            type="file"
-            accept="image/png, image/jpeg"
-            class="file-input file-input-bordered w-full max-w-xs"
-            @change="handleChange"
-            @blur="handleBlur"
-          >
+        <Field v-slot="{ handleChange, handleBlur }" name="pictureFile" rules="maxFileSize">
+          <input id="picture" type="file" accept="image/png, image/jpeg"
+            class="file-input file-input-bordered w-full max-w-xs" @change="handleChange" @blur="handleBlur">
         </Field>
         <label class="label">
-          <span class="label-text-alt text-error"><ErrorMessage name="pictureFile" /></span>
+          <span class="label-text-alt text-error">
+            <ErrorMessage name="pictureFile" />
+          </span>
         </label>
       </div>
 
-      <BaseInput
-        id="website"
-        label="個人網站"
-        name="website"
-        placeholder="https://example.com"
-      />
-      <BaseInput
-        id="linkedIn"
-        label="LinkedIn"
-        name="linkedIn"
-        placeholder="https://www.linkedin.com/in/profile/"
-        rules="validLinkedIn"
-      />
-      <BaseInput
-        id="facebook"
-        label="Facebook"
-        name="facebook"
-        placeholder="https://www.facebook.com/profile"
-        rules="validFacebook"
-      />
-      <BaseInput
-        id="instagram"
-        label="Instagram"
-        name="instagram"
-        placeholder="https://www.instagram.com/profile"
-        rules="validInstagram"
-      />
-      <BaseSelect
-        id="acceptMentoring"
-        name="acceptMentoring"
-        label="開放其他人向你諮詢"
-        help-text="若選「否」，您的資料將不會顯示在首頁"
-        :options="[{ value: '0', text: '否' }, { value: '1', text: '是' }]"
-      />
-
-      <BaseSelect
-        id="countryCode"
-        help-text=""
-        name="countryCode"
-        label="國家"
-        :options="countryOptions"
-      />
-
-      <!-- 這版先不用 Calendly -->
-      <!-- <BaseInput
-        id="calendlyUrl"
-        name="calendlyUrl"
-        label="個人預約連結"
-        placeholder="https://calendly.com/<username>"
-      > 
-        <template #helpText>
-          若選擇不提供 Calendly 連結，我們將提供您的 Email 給預約者跟您預約。
-          <a
-            class="link"
-            href="https://calendly.com/"
-          >Calendly 連結可從這申請</a>
-        </template>
-      </BaseInput> -->
-
+      <BaseInput id="website" label="個人網站" name="website" placeholder="https://example.com" />
+      <BaseInput id="linkedIn" label="LinkedIn" name="linkedIn" placeholder="https://www.linkedin.com/in/profile/"
+        rules="validLinkedIn" />
+      <BaseInput id="facebook" label="Facebook" name="facebook" placeholder="https://www.facebook.com/profile"
+        rules="validFacebook" />
+      <BaseInput id="instagram" label="Instagram" name="instagram" placeholder="https://www.instagram.com/profile"
+        rules="validInstagram" />
+      <BaseSelect id="acceptMentoring" name="acceptMentoring" label="開放其他人向你諮詢" help-text="若選「否」，您的資料將不會顯示在首頁"
+        :options="[{ value: '0', text: '否' }, { value: '1', text: '是' }]" />
+      <BaseSelect id="countryCode" help-text="" name="countryCode" label="國家" :options="countryOptions" />
       <!-- # May can use zoneinfo attribute instead -->
-      <BaseInput
-      id="timezone"
-      label="時區"
-      name="timezone"
-      placeholder="UTC+8"
-      rules="validTimezone"
-      />
+      <BaseInput id="timezone" label="時區" name="timezone" placeholder="UTC+8" rules="validTimezone" />
 
-      <BaseInput
-      id="available_time"
-      label="開放預約時間"
-      name="available_time"
-      placeholder="Every Fri-Sat, 7-8 PM, 1st week of each month"
-      rules="required"
-      />
+      <BaseInput id="available_time" label="開放預約時間" name="available_time"
+        placeholder="Every Fri-Sat, 7-8 PM, 1st week of each month" rules="required" />
 
       <div class="form-control w-full max-w-md">
         <label class="label">
           <span class="label-text">諮詢類別</span>
         </label>
-        <Field
-          v-slot="{ field }"
-          name="tags"
-        >
-          <VueMultiselect
-            v-bind="field"
-            v-model="field.value"
-            :options="state.options"
-            :multiple="true"
-            :close-on-select="false"
-            placeholder="選擇你可以提供的諮詢項目"
-          />
+        <Field v-slot="{ field }" name="tags">
+          <VueMultiselect v-bind="field" v-model="field.value" :options="state.options" :multiple="true"
+            :close-on-select="false" placeholder="選擇你可以提供的諮詢項目" />
         </Field>
       </div>
-      
-      <BaseTextarea
-        id="profileBio"
-        name="profile"
-        label="個人簡介 Bio"
-        :rows="5"
-      />
-      <button
-        type="submit"
-        class="btn btn-primary mt-2"
-        :disabled="!meta.valid || isSubmitting"
-      >
+
+      <BaseTextarea id="profileBio" name="profile" label="個人簡介 Bio" :rows="5" />
+      <button type="submit" class="btn btn-primary mt-2" :disabled="!meta.valid || isSubmitting">
         更新
       </button>
     </Form>
@@ -193,7 +78,7 @@ import jsonData from "../assets/country-iso-code-tw.json";
 
 defineRule('required', required)
 defineRule('maxFileSize', value => {
-  if (value?.size > 1024*1024*5) { // 5mb
+  if (value?.size > 1024 * 1024 * 5) { // 5MB
     return '頭像圖片尺寸不能超過 5MB'
   }
   return true
