df7204c57a4dcddd80fb3b30dfcea3d774a3d008
kennyynlin <ynlin1996@icloud.com>
2024-09-02 03:32:50 +0000

    Update forgot password form to use firebase email

--- src/components/AuthForgotPassword.vue
+++ src/components/AuthForgotPassword.vue
@@ -20,10 +20,11 @@
             {{ state.errorMessage }}
           </div>
           <BaseInput
-            name="username"
-            label="用戶名稱"
-            autocomplete="username"
-            rules="required"
+            id="email"
+            name="email"
+            label="電子信箱"
+            autocomplete="email"
+            rules="required|email"
           />
           <div class="card-actions flex-col items-center mt-2">
             <button
@@ -31,43 +32,7 @@
               class="btn btn-primary w-full"
               :disabled="!meta.valid || isSubmitting"
             >
-              發送驗證碼
-            </button>
-          </div>
-        </Form>
-      </template>
-      <template v-if="state.formState === AuthForgotPasswordState.CONFIRMATION">
-        <Form
-          v-slot="{ meta, isSubmitting }"
-          @submit="onSubmitForgotPasswordConfirm"
-        >
-          <div
-            v-if="state.errorMessage"
-            class="alert alert-error"
-            role="alert"
-          >
-            {{ state.errorMessage }}
-          </div>
-          <BaseInput
-            name="authCode"
-            autocomplete="one-time-code"
-            label="驗證碼"
-            rules="required"
-          />
-          <BaseInput
-            name="password"
-            type="password"
-            autocomplete="new-password"
-            label="新密碼"
-            rules="required|min:8"
-          />
-          <div class="card-actions flex-col items-center mt-2">
-            <button
-              type="submit"
-              class="btn btn-primary w-full"
-              :disabled="!meta.valid || isSubmitting"
-            >
-              重設密碼
+              送出
             </button>
           </div>
         </Form>
@@ -77,15 +42,17 @@
 </template>
 
 <script setup>
-import { Auth } from 'aws-amplify'
+import { auth } from '../firebase-exports'
+import { sendPasswordResetEmail } from 'firebase/auth'
 import { reactive, ref } from 'vue'
 import { useLoading } from 'vue-loading-overlay'
 import { Form, defineRule } from 'vee-validate'
-import { required } from '@vee-validate/rules'
+import { email, required } from '@vee-validate/rules'
 import { useI18n } from '../mixin/i18n'
 import BaseInput from './base/BaseInput'
 
 defineRule('required', required)
+defineRule('email', email)
 
 
 const { geti18nAuthenticationErrorMessage } = useI18n()
@@ -99,10 +66,11 @@ const AuthForgotPasswordState = Object.freeze({
 
 const formContainer = ref(null)
 const state = reactive({
-  username: '',
+  email: '',
   formState: AuthForgotPasswordState.INITIAL,
 })
 
+const emit = defineEmits(['reset-completed'])
 async function onSubmitForgotPassword(values) {
   state.errorMessage = ''
   const loader = $loading.show({
@@ -110,29 +78,13 @@ async function onSubmitForgotPassword(values) {
   })
 
   try {
-    await Auth.forgotPassword(values.username)
-
-    state.username = values.username
-    state.formState = AuthForgotPasswordState.CONFIRMATION
-
-  } catch (err) {
-    state.errorMessage = geti18nAuthenticationErrorMessage(err.message)
-
-  } finally {
-    loader.hide()
-  }
-}
-
-const emit = defineEmits(['resetCompleted'])
-async function onSubmitForgotPasswordConfirm(values) {
-  state.errorMessage = ''
-  const loader = $loading.show({
-    container: formContainer.value
-  })
-
-  try {
-    await Auth.forgotPasswordSubmit(state.username, values.authCode, values.password)
-    emit('resetCompleted')
+    await sendPasswordResetEmail(auth, values.email)
+    //TODO: show email sent message like a toast
+    setTimeout(() => {
+      emit('reset-completed')
+      state.email = values.email
+      state.formState = AuthForgotPasswordState.CONFIRMATION
+    }, 3000)
   } catch (err) {
     state.errorMessage = geti18nAuthenticationErrorMessage(err.message)
 
