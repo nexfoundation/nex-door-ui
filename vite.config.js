4e7341f6cdcf3c3d95a2a77e310e624291771556
kennyynlin <ynlin1996@icloud.com>
2024-09-12 23:52:44 +0000

    Update vercel build script

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default ({ mode }) => {
-  Object.assign(process.env, loadEnv(mode, process.cwd()));
-  return defineConfig({
-    plugins: [vue()],
-    server: {
-      host: process.env.VITE_SERVER_HOST,
-    }
-  });
-}
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true,
+  }
+});
